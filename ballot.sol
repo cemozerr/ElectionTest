cff0dd0c6869c499258223703eacbeb6cba00142
Cemozerr <cemozer2018@u.northwestern.edu>
2017-06-09 09:44:01 -0400

    ballot.sol working, ballot1.sol (which uses StandardToken) has bugs

---
+++ ballot.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.7;
+
+contract Ballot {
+    
+    mapping(bytes32 => uint) public candidates;
+    mapping(address => bool) public voted;
+    
+    event log_candidate_name(bytes32 log);
+    event log_candidate_tally(uint log);
+    
+    // candidates that are proposed by owner will be initialized to 1, 
+    // thus when somebody votes for non-proposed person, value will be 0
+    function Ballot(bytes32[] _names) {
+        for (uint i = 0; i < _names.length; i++){
+            candidates[_names[i]] = 1;
+        }
+    }
+    
+    // make sure sender has not voted yet, and the vote is to 
+    // candidate in ballot
+    // increment candidates tally
+    function Vote(bytes32 _vote){
+        require(!voted[msg.sender]);
+        require(candidates[_vote] != 0);
+        
+        candidates[_vote]++;
+        voted[msg.sender] = true;
+        
+        log_candidate_name(_vote);
+        log_candidate_tally(candidates[_vote]-1);
+    }
+    
+    
+}
---
+++ ballot1.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.7;
+
+import "HumanStandardToken.sol";
+
+contract Ballot2{
+    
+    HumanStandardToken public vote_token;
+    
+    mapping(address => bool) public voted;
+    mapping(bytes32 => uint) public candidates;
+    
+    address public govt;
+    uint256 public population;
+    bytes32[] public names;
+    
+    event debug_balance(uint256 x);
+    event debug_address(address x);
+    event log_winner(bytes32 winner);
+    event log_tie(bytes32 tie_condition);
+    
+    function Ballot2(address[] _citizens, bytes32[] _names){
+        govt = msg.sender;
+        population = _citizens.length;
+        vote_token = new HumanStandardToken(population, "vote_token", 0,"");
+        
+        for (uint32 i = 0; i < population; i++){
+            vote_token.approve(_citizens[i],1);
+            vote_token.transfer(_citizens[i],1);
+            debug_address(_citizens[i]);
+            debug_balance(vote_token.balanceOf(_citizens[i]));
+            
+        }
+        
+        for (uint32 j = 0; j < _names.length; j++){
+            candidates[_names[j]] = 1;
+            names.push(_names[j]);
+        }
+    }
+    
+    function ask_to_vote(bytes32 _vote){
+        vote_token.approve(govt,1);
+        //require(vote_token.transferFrom(msg.sender, govt, 1));
+        require(!voted[msg.sender]);
+        require(candidates[_vote] != 0);
+         
+        candidates[_vote]++;
+        voted[msg.sender] = true;
+        if (vote_token.balanceOf(govt) == population){
+            announce_winner();
+        }
+    }
+    
+    function announce_winner(){
+        bytes32 leader = "";
+        uint256 max = 0;
+        bool tie = false;
+        for (uint32 i = 0; i < names.length; i++){
+            if (candidates[names[i]] == max) {
+                tie = true;
+            }
+            else if (candidates[names[i]] > max) {
+                max = candidates[names[i]];
+                leader = names[i];
+                tie = false;
+            }
+        }
+        if (tie == true){
+            log_tie("There is a tie, repeat election");
+        }
+        else{
+            log_winner(leader);
+        }
+    }
+}
+
+
+    
+    
+
